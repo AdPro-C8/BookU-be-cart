164bfe344f38e54d9a234f971671070e72357bba
adipppp <adhif2004@gmail.com>
2024-04-17 20:34:51 +0700

    Initial commit

---
+++ src/test/java/com/adproc8/booku/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adproc8.booku.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
