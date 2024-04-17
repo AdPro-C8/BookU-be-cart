d1b5435bd2b3f0407eb14103f676d708702c0bc6
adipppp <adhif2004@gmail.com>
2024-04-17 20:36:27 +0700

    Configure project for database access

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 tasks.withType<Test> {
