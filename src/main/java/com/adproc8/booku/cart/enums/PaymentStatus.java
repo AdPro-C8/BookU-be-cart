d9c67b33d4bffa389e1042f882e35c07bb6e8c59
adipppp <adhif2004@gmail.com>
2024-04-19 17:40:41 +0700

    Refactor PaymentStatus to not use Lombok Getter

--- src/main/java/com/adproc8/booku/cart/enums/PaymentStatus.java
+++ src/main/java/com/adproc8/booku/cart/enums/PaymentStatus.java
@@ -1,16 +1,7 @@
 package com.adproc8.booku.cart.enums;
 
-import lombok.Getter;
-
-@Getter
 public enum PaymentStatus {
-    SUCCESS("SUCCESS"),
-    WAITING("WAITING"),
-    REJECTED("REJECTED");
-
-    private final String value;
-
-    private PaymentStatus(String value) {
-        this.value = value;
-    }
+    SUCCESS,
+    WAITING,
+    REJECTED;
 }
