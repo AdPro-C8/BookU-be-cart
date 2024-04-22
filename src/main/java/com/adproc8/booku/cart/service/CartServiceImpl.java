9b53c02b2201be0104e1b6bd9c4c0d5196943954
adipppp <adhif2004@gmail.com>
2024-04-22 09:40:39 +0700

    Implement CartService

---
+++ src/main/java/com/adproc8/booku/cart/service/CartServiceImpl.java
@@ -0,0 +1,33 @@
+package com.adproc8.booku.cart.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adproc8.booku.cart.model.Cart;
+import com.adproc8.booku.cart.repository.CartRepository;
+
+@Service
+public class CartServiceImpl implements CartService {
+
+    private final CartRepository cartRepository;
+
+    @Autowired
+    public CartServiceImpl(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public Cart save(Cart cart) throws IllegalArgumentException {
+        return cartRepository.save(cart);
+    }
+
+    public Optional<Cart> findById(UUID id) throws IllegalArgumentException {
+        return cartRepository.findById(id);
+    }
+
+    public void deleteById(UUID id) throws IllegalArgumentException {
+        cartRepository.deleteById(id);
+    }
+}
