326d417ca694c7de8e5a197c239d61674d728ddd
adipppp <adhif2004@gmail.com>
2024-04-22 15:44:18 +0700

    Add findByUserId query to CartRepository

--- src/main/java/com/adproc8/booku/cart/repository/CartRepository.java
+++ src/main/java/com/adproc8/booku/cart/repository/CartRepository.java
@@ -1,5 +1,6 @@
 package com.adproc8.booku.cart.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.adproc8.booku.cart.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findByUserId(UUID userId);
 }
