2da6ee3cf7c7dc5db1b7e66fcdca65cd70bcca44
adipppp <adhif2004@gmail.com>
2024-04-22 09:38:32 +0700

    Add CartRepository

---
+++ src/main/java/com/adproc8/booku/cart/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.adproc8.booku.cart.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.adproc8.booku.cart.model.Cart;
+
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+}
