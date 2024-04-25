5edd83391ef51618940f10687d274a8f1a639bab
adipppp <adhif2004@gmail.com>
2024-04-25 23:54:39 +0700

    Refactor Cart to use EmbeddedId

--- src/main/java/com/adproc8/booku/cart/model/Cart.java
+++ src/main/java/com/adproc8/booku/cart/model/Cart.java
@@ -1,7 +1,6 @@
 package com.adproc8.booku.cart.model;
 
 import java.util.List;
-import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -10,18 +9,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@IdClass(CartId.class)
 @Getter @Setter
 public class Cart {
 
     @JsonIgnore
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @EmbeddedId
+    private CartId id;
 
     @JsonIgnore
-    @Id
     @OneToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
     private User user;
 
     @ManyToMany
--- src/main/java/com/adproc8/booku/cart/model/CartId.java
+++ src/main/java/com/adproc8/booku/cart/model/CartId.java
@@ -1,13 +1,14 @@
 package com.adproc8.booku.cart.model;
 
-import java.io.Serializable;
 import java.util.UUID;
 
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
+@Embeddable
 @Getter @Setter
-public class CartId implements Serializable {
+public class CartId {
     private UUID id;
-    private User user;
+    private UUID userId;
 }
