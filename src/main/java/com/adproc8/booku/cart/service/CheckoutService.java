a401edc99e48c29fc78f34a0df3628e148258a4c
adipppp <adhif2004@gmail.com>
2024-04-25 23:50:38 +0700

    Add CheckoutService

---
+++ src/main/java/com/adproc8/booku/cart/service/CheckoutService.java
@@ -0,0 +1,12 @@
+package com.adproc8.booku.cart.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import com.adproc8.booku.cart.model.Checkout;
+
+public interface CheckoutService {
+    Checkout save(Checkout checkout);
+    Optional<Checkout> findById(UUID id);
+    Optional<Checkout> findByCartId(UUID cartId, UUID userId);
+}
