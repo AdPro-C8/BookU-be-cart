01ffea0826c69351d195ccd9fb23dab5f8d6b6b7
adipppp <adhif2004@gmail.com>
2024-05-09 18:19:40 +0700

    Update User model class to implement UserDetails

--- src/main/java/com/adproc8/booku/cart/controller/CartController.java
+++ src/main/java/com/adproc8/booku/cart/controller/CartController.java
@@ -5,12 +5,12 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adproc8.booku.cart.model.Cart;
+import com.adproc8.booku.cart.model.User;
 import com.adproc8.booku.cart.service.CartService;
 
 @RestController
@@ -25,7 +25,7 @@ class CartController {
     }
 
     @GetMapping
-    Cart getCart(@AuthenticationPrincipal UserDetails userDetails) {
+    Cart getCart(@AuthenticationPrincipal User userDetails) {
         String username = userDetails.getUsername();
 
         Cart cart = cartService.findByUsername(username)
